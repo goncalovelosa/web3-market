a31bd8b019afa47c0acaaab27c1494c4634838a2
Nino Velosa <ninovelosa@gmail.com>
2022-03-03 12:12:09 +0000

    Add configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PROJECT_ID: process.env.PROJECT_ID,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+  }
 }
 
 module.exports = nextConfig
